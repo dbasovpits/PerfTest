b71f6b32b6ae908a67f56e1b9e5a8cefad1dc8fd
d.basov@premiumitsolution.com <POLKpolk098>
2018-03-23 16:57:40 +0300

    First commit.

---
+++ src/main/java/pits/First.java
@@ -0,0 +1,12 @@
+package pits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class First {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(First.class, args);
+    }
+}
---
+++ src/main/java/pits/MainController.java
@@ -0,0 +1,14 @@
+package pits;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String welcome(Model model) {
+        return "index";
+    }
+}
